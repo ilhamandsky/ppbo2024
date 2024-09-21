dd4d8f1ecc32bb642432e5ab4d433337c5252d37
ilhamandsky <ilhammuhammad1626@gmail.com>
2024-09-21 21:26:56 +0700

    Praktikum 6E - Menggunakan Pewarisan

--- App/Admin/Dosen.php
+++ App/Admin/Dosen.php
@@ -6,6 +6,6 @@ class Dosen extends Pegawai{
     public $nidn;
     
     public function mengajar() : void {
-        echo "{$this->nama} sedang mengajar perkuliahan.";
+        echo "{$this->nama} sedang mengajar perkuliahan";
     }
  }
--- App/Admin/TenagaKependidikan.php
+++ App/Admin/TenagaKependidikan.php
@@ -1,25 +1,11 @@
 <?php
 namespace App\Admin;
+require_once 'App/Admin/Pegawai.php';
 
-class Pegawai {
-    public $nip;
-    public $nama;
-    protected $no_hp;
-    public $alamat;
-
-    public function cekIn() : bool {
-        return true;
-    }
+class TenagaKependidikan extends Pegawai{
+    public $gaji_pokok;
     
-    public function cekOut() : bool {
-        return true;
-    }
+    public function cuti() : void {
 
-    public function getNoHp() : int {
-        return $this->no_hp;
-    }
-
-    public function setNoHp($no_hp) : void {
-        $this->no_hp = $no_hp;
     }
 }
